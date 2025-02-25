9484c3516943747eeb2fe749a02b6c66c9054ea3
eva672 <evamanuska@gmail.com>
2025-02-25 17:52:06 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
-}
+    use std::env;
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: {} <enable_fib> <max_threshold>", args[1]);
+        return;
+    }
+
+    let enable_fib = &args[1];
+    let max_threshold = &args[2];
+
+    println!("\n enable_fib: {}", enable_fib);
+    println!("\n max_threshold: {}", max_threshold);
+
+    if enable_fib == "true" {
+        // Your Fibonacci logic here
+        println!("\n Fibonacci program is enabled with max threshold: {}", max_threshold);
+    } else {
+        println!("\n Fibonacci program is disabled");
+    }
+} 
+
