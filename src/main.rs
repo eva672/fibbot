a8cb055bfd8e00a5fc6db1cd4a3b1a79ed0da2f6
eva672 <evamanuska@gmail.com>
2025-02-28 16:51:22 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
-fn main() {
+use std::env;
+
+use octocrab::{models::{repos::DiffEntry, pulls::PullRequest, repos::Content}, Octocrab, Page};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error >>{
+    let github_repository = env::var("GITHUB_REPOSITORY").unwrap_or_else(|_| "eva672/fibbot".to_string());
+    let github_repository=  github_repository.split("/").collect::<Vec<&str>>();
+    let owner = github_repository[0];
+    let repo = github_repository[1];
+
+let pr = octocrab::instance().pulls( owner,  repo).list_files(1).await?;
+println!("{:?}", pr);
+let path = &pr.items.first().unwrap().patch.clone().unwrap();
+let numbers= extract_numbers::extract_numbers(&path);
+println!("{:?}",numbers);
+let result=main2();
+println!("{:?}",result);
+
+Ok(()) 
+
+}
+fn main2() {
     println!("Hello, world!");
     use std::env;
 
@@ -19,6 +41,13 @@ fn main() {
         println!("\n Fibonacci program is enabled with max threshold: {}", max_threshold);
     } else {
         println!("\n Fibonacci program is disabled");
+        
     }
+    
 } 
+
+
+
+mod extract_numbers;
+mod pull_request;
 
