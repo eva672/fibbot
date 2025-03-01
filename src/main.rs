e793556e738c196269b9a53465dfb618d1d0de30
eva672 <evamanuska@gmail.com>
2025-03-01 17:18:36 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let owner = github_repository[0];
     let repo = github_repository[1];
 
-    let pr = octocrab::instance().pulls(owner, repo).list_files(1).await?;
+    let pr = octocrab::instance().pulls(owner, repo).list_files(3).await?;
     println!("{:?}", pr);
     let path = &pr.items.first().unwrap().patch.clone().unwrap();
     let numbers = extract_numbers(&path);
@@ -34,7 +34,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     if enable_fib == "true" {
         // Your Fibonacci logic here
-        let pr = octocrab::instance().pulls(owner, repo).list_files(1).await?;
+        let pr = octocrab::instance().pulls(owner, repo).list_files(3).await?;
         println!("{:?}", pr);
         let path = &pr.items.first().unwrap().patch.clone().unwrap();
         let numbers = extract_numbers(&path);
